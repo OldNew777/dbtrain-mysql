95b9b0f462084bde50c7fe3847c449114c66651f
ChenXin <chenxin18@mails.tsinghua.edu.cn>
2021-12-23 00:53:41 +0800

    add schema

---
+++ src/table/schema.h
@@ -0,0 +1,23 @@
+#ifndef DBTRAIN_MYSQL_SCHEMA_H
+#define DBTRAIN_MYSQL_SCHEMA_H
+
+#include "defines.h"
+#include "table/column.h"
+
+namespace dbtrain_mysql {
+
+class Schema {
+ public:
+  Schema(const std::vector<Column>& iColVec);
+  ~Schema() = default;
+
+  Size GetSize() const;
+  Column GetColumn(Size nPos) const;
+
+ private:
+  std::vector<Column> _iColVec;
+};
+
+}  // namespace dbtrain_mysql
+
+#endif
